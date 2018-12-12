b574de1d6bd596d3dca024f5c45bb6b5825d024d
Sam <contactsamie@gmail.com>
2018-12-12 06:49:19 -0800

    initial commir

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperApp.UILaboratory
+{
+    using System.Diagnostics;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Serialization;
+    using ServeMeLib;
+
+   public  class Program
+    {
+        public string GetSomething(string arg)
+        {
+            return JsonConvert.SerializeObject(new
+            {
+                Date=DateTime.UtcNow,
+                How="I will Do it"
+            });
+        }
+        static void Main(string[] args)
+        {
+            string instruction = ServeMe.GetMethodExecutionInstruction(typeof(Program), nameof(GetSomething));
+            string serverCsv = $"equalto /search,assembly {instruction},get\napp log";
+            using (var serveMe = new ServeMe())
+            {
+                string url = serveMe.Start(serverCsv).First();
+                Process.Start(url);
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ app.js
@@ -0,0 +1,31 @@
+﻿var app = angular.module(name, ['ui.router']);
+app.config(['$urlRouterProvider', '$stateProvider', function ($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('home', {
+            url: '/',
+            views: {
+                'body': {
+                    template: '<div template>we did it</div>'
+                },
+                'footer': {
+                    template: "<div class='vCentered'>All rights reserved</div>"
+                }
+            }
+        });
+}]);
+app.directive('template', function () {
+    return {
+        restrict: 'EA',
+        transclude: true,
+        template: 'template for <ng-transclude></ng-transclude>',
+        controller: function ($scope, $rootScope, $timeout, $interval) {},
+        link: function(scope, element, attributes) {
+            $(element).append("<div id='time'></div>");
+            $('#time').load("/search");
+            console.log(t);
+        }
+    };
+});
+
+angular.bootstrap(document, [name]);
